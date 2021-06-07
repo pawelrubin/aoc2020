3b74fc487d5d0253cca4a46101278773fbd10e76
Paweł Rubin <pawelrubin19@gmail.com>
2021-06-07 23:41:18 +0200

    Day 1, Ada: Report Repair. Part one.

---
+++ 01_ada/report_repair_part_one.adb
@@ -0,0 +1,29 @@
+with Ada.Text_IO;    use Ada.Text_IO;
+with Ada.Containers; use Ada.Containers;
+with Ada.Containers.Ordered_Sets;
+
+procedure Report_Repair_Part_One is
+   package Positive_Sets is new Ada.Containers.Ordered_Sets
+     (Element_Type => Positive);
+
+   use Positive_Sets;
+   year       : constant Integer := 2_020;
+   F          : File_Type;
+   File_Name  : constant String  := "input.txt";
+   seen       : Set;
+   num        : Positive;
+   complement : Positive;
+   product    : Positive;
+begin
+   Open (F, In_File, File_Name);
+   while not End_Of_File (F) loop
+      num        := Integer'Value (Get_Line (F));
+      complement := year - num;
+      if seen.Contains (complement) then
+         product := (num * complement);
+         Put_Line (product'Img);
+      end if;
+      seen.Insert (num);
+   end loop;
+   Close (F);
+end Report_Repair_Part_One;
